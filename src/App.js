d733a1ce9a16b0b3546370f19d1e3eaee7bf7e90
ocnow <ockulkarni97@gmail.com>
2023-01-11 23:31:05 +0530

    basic login-page created

--- src/App.js
+++ src/App.js
@@ -4,29 +4,9 @@ import './login/login'
 import Login from './login/login';
 
 
-function LoadAppButton() {
-  return (
-    <button onClick={DoThis}> Load Application </button>
-  );
-}
-
-function DoThis() {
-  return (
-    <Login />
-  );
-}
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-          <LoadAppButton />
-        
-        
-      </header>
-    </div>
+    <Login />
   );
 }
 
--- src/login/login.js
+++ src/login/login.js
@@ -1,7 +1,27 @@
-function Login(){
+import logo from '../logo.svg';
+
+function LoginButton(){
     return (
-        <div>
-            <h1>This is the login page</h1>
+        <button className="loginButton" onClick={loginUser}> Login</button>
+    )
+}
+
+function loginUser(){
+    console.log("User logged in!")
+}
+
+function Login(){
+    var Text = "text";
+    var password = "password";
+    return (
+        <div className="AppPage">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>Login Page</h1>
+                <label htmlFor="uname"> User Name </label>
+                <input id="uname" type={Text} placeholder="userName"/> <br/>
+                <label htmlFor="pwd"> password </label>
+                <input id="pwd" type={password} placeholder="password"/> <br/>
+                <LoginButton />
         </div>
     );
 }
